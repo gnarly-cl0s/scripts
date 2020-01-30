d4f0c120ea71330d59f232229e19464c78249bc5
Carlos M <49469448+gnarly-cl0s@users.noreply.github.com>
2020-01-30 15:45:58 -0700

    error handling
    Included error handling for each apt process and clean up

--- Update.sh
+++ Update.sh
@@ -1,11 +1,74 @@
 #!/bin/bash
-# if not root, run as root
+
+#if not root, run as root
 if (( $EUID != 0 )); then
-    sudo $HOME/git/scripts/Update.sh
-    exit
+        sudo $HOME/up.sh
+        exit
 fi
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get clean
-apt-get -y autoremove
+
+check_exit_status() {
+
+        if [ $? -eq 0 ]
+        then
+                echo
+                echo "Success"
+                echo
+        else
+                echo
+                echo "[ERROR] Process Failed!"
+                echo
+
+                read -p "The last command exited with an error. Exit script? (yes/no) " answer
+
+            if [ "$answer" == "yes" ]
+            then
+                exit 1
+            fi
+        fi
+}
+
+greeting() {
+
+        echo
+        echo "Hello, $USER. Let's update this system."
+        echo
+}
+
+update() {
+
+        sudo apt-get update;
+        check_exit_status
+
+        sudo apt-get upgrade -y;
+        check_exit_status
+
+        sudo apt-get dist-upgrade -y;
+        check_exit_status
+}
+
+housekeeping() {
+
+        sudo apt-get autoremove -y;
+        check_exit_status
+
+        sudo apt-get autoclean -y;
+        check_exit_status
+
+        sudo updatedb;
+        check_exit_status
+}
+
+leave() {
+
+        echo
+        echo "--------------------"
+        echo "- Update Complete! -"
+        echo "--------------------"
+        echo
+        exit
+}
+
+greeting
+update
+housekeeping
+leave
